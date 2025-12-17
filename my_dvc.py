d6981667298bbf37f40f5e8da6449202ddbe10af
Ankit Show <ankitshow005@gmail.com>
2025-12-17 12:23:17 +0530

    Started tracking raw data 1st version with dvc

--- my_dvc.py
+++ my_dvc.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('C:/Users/Sanjoy/Practice/dvc_practice/data_location/raw_data.csv')
-
+git 
 cat_col = [col for col in df.columns if df[col].dtype == 'O'] 
 num_col = [col for col in df.columns if df[col].dtype != 'O']
 
