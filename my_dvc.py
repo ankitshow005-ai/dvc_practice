acf6f71ff54d6941d96e8a7052c81be851cd7936
Ankit Show <ankitshow005@gmail.com>
2025-12-17 12:50:37 +0530

    2nd version of data

--- my_dvc.py
+++ my_dvc.py
@@ -2,8 +2,15 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('C:/Users/Sanjoy/Practice/dvc_practice/data_location/raw_data.csv')
-git 
-cat_col = [col for col in df.columns if df[col].dtype == 'O'] 
-num_col = [col for col in df.columns if df[col].dtype != 'O']
 
-print(cat_col, num_col)
+# cat_col = [col for col in df.columns if df[col].dtype == 'O'] 
+# num_col = [col for col in df.columns if df[col].dtype != 'O']
+
+# print(f'{cat_col}, {num_col}') 
+
+data = pd.DataFrame({'name': ['Ankit'], 'age': [23], 'city': ['Kolkata']})
+print(data)
+
+new_df = pd.concat([df,data], ignore_index = True)
+print(new_df)
+
