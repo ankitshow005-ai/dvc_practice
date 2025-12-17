4173ac34dfa6f946d8eccb7e093217e7fdeff857
Ankit Show <ankitshow005@gmail.com>
2025-12-17 12:03:03 +0530

    Added data file

---
+++ my_dvc.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('C:/Users/Sanjoy/Practice/dvc_practice/data_location/raw_data.csv')
+
+cat_col = [col for col in df.columns if df[col].dtype == 'O'] 
+num_col = [col for col in df.columns if df[col].dtype != 'O']
+
+print(cat_col, num_col)
